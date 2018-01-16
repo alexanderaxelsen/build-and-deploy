7ba97405bb7641440b8dc4871590996e2c1a3c9a
Alexander Axelsen <alexander.axelsen@bouvet.no>
2018-01-16 14:31:37 +0100

    staging and deploy

--- src/calcCtrl.js
+++ src/calcCtrl.js
@@ -1,35 +1,32 @@
-(function(){
-  'use strict';
+(function () {
+    'use strict';
 
-  angular.module('calcApp').controller('calcCtrl', CalcController);
+    angular.module('calcApp').controller('calcCtrl', CalcController);
 
-  CalcController.$inject = [];
+    CalcController.$inject = [];
 
-  function CalcController(){
-    var vm = this;
+    function CalcController() {
+        var vm = this;
 
-    vm.result = '';
-    vm.calculate = calculate;
-    vm.oper = 'plus';
+        vm.result = '';
+        vm.calculate = calculate;
+        vm.oper = 'plus';
 
-    function calculate(firstArg, secondArg, operator){
-      if(operator === 'plus'){
-        var result = firstArg + secondArg;
-        vm.result = result;
-      //} else if (operator === 'minus'){
-      //  var result = firstArg - secondArg;
-      //  vm.result = result;
-      } else if(operator === 'divide'){
-        if (secondArg !== 0){
-          var result = firstArg / secondArg;
-          vm.result = result;
-        } else {
-          vm.result = 'Cannot divide by zero'
+        function calculate(firstArg, secondArg, operator) {
+            if (operator === 'plus') {
+                vm.result = firstArg + secondArg;
+            } else if (operator === 'minus') {
+                vm.result = firstArg - secondArg;
+            } else if (operator === 'divide') {
+                if (secondArg !== 0) {
+                    vm.result = firstArg / secondArg;
+                } else {
+                    vm.result = 'Cannot divide by zero'
+                }
+            }
+            else {
+                vm.result = "not implemented"
+            }
         }
-      }
-      else {
-        vm.result = 'Operator not implemented';
-      }
-    }
-  };
+    };
 }());
